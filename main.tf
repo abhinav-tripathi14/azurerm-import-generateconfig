c4c220f9434d86a63a3c499c90347418e2823345
Abhinav Tripathi <abhi.tripathi14@gmail.com>
2024-12-09 07:49:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ provider "azurerm" {
 features { }
 
 }
+
+import {
+  to = azurerm_resource_group.rg
+  id = "/subscriptions/9f9b362c-0ced-42f9-8327-987494fd7c26/resourceGroups/example-resources-test"
+}
